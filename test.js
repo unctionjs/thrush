bec2027f4882028b7977534a7aa4dacfcca5c5c6
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-27 18:51:03 -0700

    Going back to how other javascript packages do it, makes it eaiser on everyone :/

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers */
 import {test} from "tap"
 
-import thrush from "./source"
+import thrush from "./"
 
 test(({equal, end}) => {
   equal(
