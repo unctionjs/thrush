e4cbd3f2491d2acc33f297cb32d1f453422e67d0
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-28 00:50:52 -0700

    Now nyc will use the non-transpiled version

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers */
 import {test} from "tap"
 
-import thrush from "./"
+import thrush from "./index"
 
 test(({equal, end}) => {
   equal(
